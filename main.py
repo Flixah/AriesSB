cb5b10010d2be4f3180c4b86cacbdbbae7a251b5
Nshout <nshout99@gmail.com>
2021-12-28 23:21:03 +0100

    Added imports

---
+++ main.py
@@ -0,0 +1,46 @@
+import os
+import re
+import sys
+import json
+import time
+import httpx
+import base64
+import qrcode
+import dhooks
+import string
+import socket
+import urllib
+import ctypes
+import random
+import discum
+import psutil
+import typing
+import aiohttp
+import asyncio
+import discord
+import hashlib
+import pwinput
+import requests
+import bitly_api
+import threading
+import pyPrivnote
+import subprocess
+import pypresence
+import webbrowser
+import ctypes.wintypes as wintypes
+from CEA256 import *
+from gtts import gTTS
+from discord import *
+from ctypes import windll
+from pytube import YouTube
+from notifypy import Notify
+from os import error, name, system
+from datetime import datetime
+from licensing.models import *
+from pypresence import Presence
+from discord.ext import commands
+from urllib.request import urlopen
+from urllib.parse import quote_plus
+from time import localtime, strftime
+from licensing.methods import Key, Helpers
+from discord.ext.commands import MissingPermissions, CheckFailure, CommandNotFound, has_permissions
